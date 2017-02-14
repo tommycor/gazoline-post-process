de078e65aaf1d889792f421d87449d6bd1c4cd2e
tommycor <tommycornilleau@gmail.com>
2017-02-14 01:42:44 +0100

    add post process shader

--- src/scripts/components/scene.js
+++ src/scripts/components/scene.js
@@ -1,15 +1,33 @@
-import config 				from '../utils/config';
-import raf 					from '../utils/raf';
-import mapper 				from '../utils/mapper';
+import config 		from '../utils/config';
+import raf 			from '../utils/raf';
+import mapper 		from '../utils/mapper';
+import GlslCanvas	from 'glslCanvas'
 
 module.exports = {
 
 	init: function() {
+		this.onResize	= this.onResize.bind(this);
+		this.onMove		= this.onMove.bind(this);
+		this.onClick	= this.onClick.bind(this);
+		this.container 	= document.querySelector('#container');
+		
+		//get context
+		this.canvas = document.createElement("canvas");
+		this.onResize();
+		this.ctx 	= new GlslCanvas(this.canvas);
+
+		this.fragShader = require('../shaders/water.fragment.glsl');
+		// this.vertexShader = require('../shaders/water.vertex.glsl');
+
+		this.ctx.load( this.fragShader );
+		// this.ctx.load(string_frag_code);
+
+
+		this.container.appendChild(this.canvas);
+
 		//// REGIST RENDERER
 		raf.register( this.render );
 		raf.start();
-		this.onResize();
-		this.addControls();
 
 		window.addEventListener( 'resize', this.onResize );
 		window.addEventListener( 'mousemove', this.onMove );
@@ -23,14 +41,14 @@ module.exports = {
 	},
 
 	onResize: function() {
+		this.canvas.width = this.container.offsetWidth;
+		this.canvas.height = this.container.offsetHeight;
+
 		this.halfWidth = window.innerWidth * .5;
 		this.halfHeight = window.innerHeight * .5;
 	},
 
 	render: function() {
-		let delta = this.clock.getDelta();
-
-		this.renderer.render(this.scene, this.camera);
 	}
 
 };
---
+++ src/scripts/shaders/water.fragment.glsl
@@ -0,0 +1,10 @@
+precision highp float;
+precision highp int;
+
+uniform float u_time;
+uniform float u_resolution;
+uniform float u_mouse;
+
+void main() {
+	gl_FragColor = vec4( 0., 1., 1., 1. );
+}
