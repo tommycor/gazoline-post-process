4ad34b9e9923acda61351fee466ea4362d7ce94f
Tommy Cornilleau <tommy.cornilleau@colorz.fr>
2017-02-23 13:57:42 +0100

    gently start adding interactions

--- src/scripts/shaders/water.fragment.glsl
+++ src/scripts/shaders/water.fragment.glsl
@@ -10,9 +10,9 @@ vec3 rgb = vec3(.0, .0, .0);
 
 void main() {
 	vec3 noise = vec3(
-		snoise( vec3( vUv * 2. + offset.r, u_time * .5 ) ) * .25 + .5,
-		snoise( vec3( vUv * 2. + offset.g, u_time * .5 ) ) * .25 + .5,
-		snoise( vec3( vUv * 2. + offset.b, u_time * .5 ) ) * .25 + .5
+		snoise( vec3( vUv * 2. + offset.r, u_time * .5 ) ) * .5 + .5,
+		snoise( vec3( vUv * 2. + offset.g, u_time * .5 ) ) * .5 + .5,
+		snoise( vec3( vUv * 2. + offset.b, u_time * .5 ) ) * .5 + .5
 	);
 
 	vec3 rgb = texture2D(u_tex, vUv).rgb * noise;
