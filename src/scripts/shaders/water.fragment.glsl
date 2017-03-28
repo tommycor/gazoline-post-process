9fb7a480e4af2554a1cc93950b9ffc8f02a1b12e
Tommy Cornilleau <tommy.cornilleau@colorz.fr>
2017-03-28 14:30:39 +0200

    refacto fragment shader

--- src/scripts/shaders/water.fragment.glsl
+++ src/scripts/shaders/water.fragment.glsl
@@ -23,28 +23,59 @@ vec3 rgb 	= vec3(.0, .0, .0);
 vec2 diff 	= vec2(.0, .0);
 
 
-vec4 getWaveValue( vec2 interactionsPos, float interactionsTime ) {
+vec4 getWaveValue( vec2 interactionsPos, float interactionsTime, int ponderation ) {
+	
+	vec3 sinVal = vec3( .0, .0, .0);
+
 	float dist  = .0;
 	float influence = .0;
-	float influenceSlope = -.08;
 	float influenceTime = .0;
-	float displacement = .0;
-	float frequency = 2.5;
-	float amplitude = .2;
-	float waveLength = .5;
+	float influenceSlope = .0;
+	float frequency = .0;
+	float amplitude = .0;
+	float waveLength = .0;
 	float shift = .0;
 
-	vec3 sinVal = vec3( .0, .0, .0);
+	if( ponderation == 0 ) {
+
+		influenceSlope = -.08;
+		frequency = 2.5;
+		amplitude = .2;
+		waveLength = .5;
+		shift = .0;
+
+	}
+	else if( ponderation == 1 ) {
+
+		influenceSlope = -.08;
+		frequency = 4.;
+		amplitude = 3.5;
+		waveLength = .2;
+		shift = .0;
+	}
+
 
 	dist = distance( vec3( interactionsPos, .0 ), vec3( vPosition.xy , 0.) );
 
-	// INFLUENCE FROM DIST + SPAWNING 
-	if( interactionsTime < 2. && dist < MAX_DIST ) {
-		influence = ( dist * influenceSlope ) + interactionsTime * .7 + .2;
+	if( ponderation == 0 ) {
+		// INFLUENCE FROM DIST + SPAWNING 
+		if( interactionsTime < 2. && dist < MAX_DIST ) {
+			influence = ( dist * influenceSlope ) + interactionsTime * .7 + .2;
+		}
+
+		// FADE OUT
+		influenceTime = ( interactionsTime * -.5 + 1. );
+	}
+	else if( ponderation == 1 ) {
+		// INFLUENCE FROM DIST + SPAWNING 
+		if( interactionsTime < 4. && dist < MAX_DIST + 10. ) {
+			influence = ( dist * influenceSlope ) + interactionsTime * .7 + .0;
+		}
+
+		// FADE OUT
+		influenceTime = ( interactionsTime * -.33 + 1. );
 	}
 
-	// FADE OUT
-	influenceTime = ( interactionsTime * -.5 + 1. );
 
 	if( influenceTime > .0 ) {
 
@@ -57,56 +88,11 @@ vec4 getWaveValue( vec2 interactionsPos, float interactionsTime ) {
 			sinVal = sin( ( dist * waveLength - interactionsTime * frequency ) + offsetWave ) * amplitude + shift;
 
 			sinVal = sinVal * influence;
-
-			// rgb = rgb + rgb * sinVal;
 		}
 	}
 
-	return vec4( sinVal, dist );
+	return vec4( sinVal, dist );		
 }
-
-vec4 getBigWaveValue( vec2 interactionsPos, float interactionsTime ) {
-
-	float dist  = .0;
-	float influence = .0;
-	float influenceSlope = -.08;
-	float influenceTime = .0;
-	float displacement = .0;
-	float frequency = 4.;
-	float amplitude = 3.5;
-	float waveLength = .2;
-	float shift = .0;
-
-	vec3 sinVal = vec3( .0 );
-
-	dist = distance( vec3( interactionsPos, .0 ), vec3( vPosition.xy , 0.) );
-
-	// INFLUENCE FROM DIST + SPAWNING 
-	if( interactionsTime < 4. && dist < MAX_DIST + 10. ) {
-		influence = ( dist * influenceSlope ) + interactionsTime * .7 + .0;
-	}
-
-	// FADE OUT
-	influenceTime = ( interactionsTime * -.33 + 1. );
-
-	if( influenceTime > .0 ) {
-
-		influence = influence * influenceTime ;
-
-		// influence is gonna act on simili sombrero function
-		if( influence > .0 ) {
-
-			// HERE WE ONLY CALCULATE REAL WAVE
-			sinVal = cos( ( dist * waveLength - interactionsTime * frequency ) + offsetWave ) * amplitude + shift;
-
-			sinVal = sinVal * influence;
-		}
-	}
-
-	return vec4( sinVal, dist );
-}
-
-
 
 
 void main() {
@@ -124,20 +110,12 @@ void main() {
 
 	vec2 explosions = vec2( 0. );
 
-
 	for( int i = 0 ; i < MAX_INT ; i++ ) {
 		if( i >= uInteractionsIndex ) {
 			break;
 		}
 
-		sinVal = vec4( 0. );
-
-		if( uInteractionsPonderation[i] == 0 ) {
-			sinVal = getWaveValue( uInteractionsPos[i], uInteractionsTime[i] );
-		}
-		else if( uInteractionsPonderation[i] == 1 ) {
-			sinVal = getBigWaveValue( uInteractionsPos[i], uInteractionsTime[i] );
-		}
+		sinVal = getWaveValue( uInteractionsPos[i], uInteractionsTime[i], uInteractionsPonderation[i] );
 
 		if( sinVal.rgb == vec3( 0 ) ) { continue; }
 
@@ -148,6 +126,7 @@ void main() {
 	}
 
 	rgb = texture2D(uTex, vUv + explosions * .004 ).rgb * noise;
+	// rgb = texture2D(uTex, vUv ).rgb * noise;
 
 	rgb = rgb * globalSinVal;
 
--- src/scripts/utils/config.js
+++ src/scripts/utils/config.js
@@ -39,7 +39,7 @@ var config = {
 	plane: {
 		width: 100,
 		height: 50,
-		segments: 20
+		segments: 2
 	}
 }
 
